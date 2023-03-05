ed3c59f2404cf643c9e491f26a12fcf7ed8b47a4
DimiVaksman <vde92@hotmail.com>
2023-03-05 11:56:19 +0100

    допрацювання клятого промису

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,22 +9,19 @@ const alertStyle = {
   cssAnimationStyle: 'from-left',
 }
 
-const create = document.querySelector('button')
-console.log(create)
-const form = document.querySelector('.form.form')
-console.log(form)
-const delay = document.querySelector('[name="delay"]')
-console.log(delay)
-const step = document.querySelector('[name="step"]')
-console.log(step);
-const amount = document.querySelector('[name="amount"]')
-console.log(amount);
 const body = document.querySelector('body')
 body.style.backgroundColor = '#0af7ff'
 
+const refs = {
+  body: document.querySelector('body'),
+  form: document.querySelector('form.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+}
 
 
-form.addEventListener('click' , startPromise );
+refs.form.addEventListener('click' , startPromise );
 
 function createPromise(position, delay) {
   return new Promise ((resolve, reject) => {
@@ -44,24 +41,30 @@ function createPromise(position, delay) {
 function startPromise(e){
 e.preventDefault();
 
-let inputDelay = Number(delay.value);
-let inputStep = Number(step.value);
-let inputAmount = Number(amount.value);
+let inputDelay = Number(refs.delay.value);
+let step = Number(refs.step.value);
+let amount = Number(refs.amount.value);
 
-for (let i = 0; i < inputAmount; i += 1) {
-  inputDelay += inputStep * i;
+// for (let i = 1; i <= amount; i += 1) {                 Не зрозумію, що зробити , для того щоб правильно виконувалась позиція!!!!!!!!
+//  let inputProm = inputDelay + step * i;
+
+ for (let i = 0; i < amount; i += 1) {
+  let inputProm = inputDelay + step * i;
 
 
   
-createPromise(i, inputDelay).then(({position, delay}) =>  {
+createPromise(i, inputProm)
+.then(({position, delay}) =>  {
   Notify.success(
     `✅ Fulfilled promise ${position} in ${delay}ms`, alertStyle
   )
+  // window.alert(`✅ Fulfilled promise ${position} in ${delay}ms`)
 })
 .catch(({position, delay})=>{
   Notify.failure(
     `❌ Rejected promise ${position} in ${delay}ms`, alertStyle
   )
+  // window.alert(`❌ Rejected promise ${position} in ${delay}ms`)
 })
 
 }
